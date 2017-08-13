39ea9797e6c3161e4a41b08f0dae4fcbd142e3e9
ahmedhamdi <ahmedhamdi@Ahmed-Hamdi>
2017-08-13 16:17:53 +0200

    First Commit

---
+++ UserCreation/src/main/java/newUsers/Base.java
@@ -0,0 +1,234 @@
+package newUsers;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Base {
+	
+	public static String baseURL = "";
+	public static WebDriver driver = null;
+	private final String USER_AGENT = "Mozilla/5.0";
+	private boolean CheckNameExists= false;
+	private String strRandom = "";
+	Boolean isPresent;
+	static Boolean isWindows= false;
+
+	public static WebDriver LoadDriver(WebDriver driver, String DriverName)
+	{
+		if (isWindows ==false)
+		{
+			// Setup FireFox and Chrome Driver for Linux. 
+			if (DriverName == "F")
+			{
+				System.setProperty("webdriver.gecko.driver", "src/drivers/geckodriver");
+				ProfilesIni profile = new ProfilesIni();
+				FirefoxProfile Uprofile = profile.getProfile("default");
+				Uprofile.setAcceptUntrustedCertificates(true);
+				Uprofile.setAssumeUntrustedCertificateIssuer(true);
+				Uprofile.setPreference("security.insecure_field_warning.contextual.enabled", false);
+				DesiredCapabilities cap = DesiredCapabilities.firefox();
+				cap.setCapability(FirefoxDriver.PROFILE, Uprofile);
+				cap.setCapability("marionette", true);
+				driver = new FirefoxDriver(cap);
+				return driver;
+			}
+			else if (DriverName == "C")
+			{
+				System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver");
+				driver = new ChromeDriver();
+				return driver;
+			}
+			else 
+			{
+				System.out.println("Please Enter Correct Driver Name: F or C");
+				driver = null;
+			}
+		}
+		else 
+		{
+			// Setup FireFox and Chrome Driver for Windows. 
+						if (DriverName == "F")
+						{
+							System.setProperty("webdriver.gecko.driver", "src/drivers/geckodriver.exe");
+							driver = new FirefoxDriver();
+							return driver;
+						}
+						else if (DriverName == "C")
+						{
+							System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+							driver = new ChromeDriver();
+							return driver;
+						}
+						else 
+						{
+							System.out.println("Please Enter Correct Driver Name: F or C");
+							driver = null;
+						}
+		}
+		return driver;
+	}
+	
+	public void FillUserData(WebDriver driver, String varName, String varEmail, String varPassword, String varConfirmPassword, Boolean varSubmit){
+		// Fill in User Data( Web Driver, User Name, User Email, Password, Confirm Password, Click on button Submit or Not) 
+
+		driver.findElement(By.id("name")).sendKeys(varName);
+		driver.findElement(By.id("email")).sendKeys(varEmail);
+		driver.findElement(By.id("password")).sendKeys(varPassword);
+		driver.findElement(By.id("confirmationPassword")).sendKeys(varConfirmPassword);
+		if (varSubmit == true){
+			driver.findElement(By.xpath("/html/body/div/div/div/form/fieldset/div[5]/button")).click();
+		} else { /*Do Nothing*/}
+	}
+
+	public Boolean checkNameIsUnique(String varName) throws Exception{
+		
+		checkNameorEmailExists(baseURL+"/user/all/json", varName, "");
+		return CheckNameExists;	
+	}
+	
+	public Boolean checkEmailIsUnique(String varEmail) throws Exception{
+		
+		checkNameorEmailExists(baseURL+"/user/all/json", "", varEmail);
+		return CheckNameExists;	
+	}
+
+	public String sendGet(String varURL) throws Exception {
+
+		// Use Web Service Action GET, to retrieve all Users data 
+			String url = varURL; 
+
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// optional default is GET
+			con.setRequestMethod("GET");
+
+			//add request header
+			con.setRequestProperty("User-Agent", USER_AGENT);
+
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			return response.toString();
+
+		}
+	
+	public boolean checkNameorEmailExists(String varURL, String varName, String varEmail) throws Exception {
+
+			// Select data of All Users
+			String strResponse=sendGet(varURL);
+			
+
+			//print result
+			System.out.println(strResponse.toLowerCase()+"\n");
+			
+			// Check Email or Name exists according to Method parameter 
+			
+			if (varEmail == ""){
+			System.out.println(varName.toLowerCase());
+			return CheckNameExists= strResponse.toLowerCase().contains(varName.toLowerCase());
+			}
+			else if (varName == ""){
+			System.out.println(varEmail.toLowerCase());
+			return CheckNameExists= strResponse.toLowerCase().contains(varEmail.toLowerCase());
+			}
+			else
+			{
+				return CheckNameExists=false;
+			}
+
+		}
+	
+	public void sendDelete(String varURL) throws Exception {
+
+		String url = varURL; 
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		con.setRequestMethod("DELETE");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'Delete' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	public String RandomValue(int min, int max, String strPrefix, Boolean IfEmail){
+		int ValueToAddedToEmailOne = ThreadLocalRandom.current().nextInt(min, max + 1);
+		
+		if (IfEmail){
+			strRandom = strPrefix+"@R"+ValueToAddedToEmailOne+".com";
+		}
+		else
+		{
+			strRandom = strPrefix+"R"+ValueToAddedToEmailOne;
+		}
+				return strRandom;
+	}
+
+	public boolean AssertElement(String elementId, String ExpectedMessage)
+	{
+	Boolean Passed = false;	
+	try{
+		isPresent = driver.findElement(By.id(elementId)).getText().isEmpty();
+	} catch(Exception e){
+		isPresent = true;
+	}
+	
+	if (isPresent)
+	{
+		Passed = false;
+	}else
+	{
+		String ActualMessage = driver.findElement(By.id(elementId)).getText().toString();
+		if (ExpectedMessage == ActualMessage)
+		{
+			Passed = true;
+		}else
+		{
+			Passed = false;
+		}	
+	}
+	return Passed;
+	}
+
+}
---
+++ UserCreation/src/test/java/newUsers/UserDataTest.java
@@ -0,0 +1,249 @@
+package newUsers;
+
+import static org.junit.Assert.fail;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class UserDataTest extends Base {
+
+	String NameErrorRequired, NameErrorUnique, UserNameOne, UserNameOneRepeated,
+	strValueOfEmailOne, strValueOfEmailTwo
+	
+	,EmailErrorRequired,EmailErrorUnique, strEmail, strEmailRepeated, 
+	strValueToAddToNameOne, strValueToAddToNameTwo
+	
+	,strValueToMakeUniqueValues
+
+	,strValueOfName,strValueOfEmail
+	= null;
+	
+	// BE CAUTIOUS, IF THIS VALUE IS TRUE, ALL USERS WILL BE DELETED WHEN RUNNING TEST CASES. 
+	Boolean UsingWebServiceDelete = false; 
+	
+	public void LoadBrowser(){
+		// Load WebDriver C for Chrome and F for FireFox 
+		driver = LoadDriver(driver, "F");
+		// Open the URL
+		driver.get(baseURL);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void UserNameIsRequired(){
+				// Open Browser
+				LoadBrowser();
+
+				// Test Case #1: User Name is required. 
+				// Fill in all data but set user name to be NULL
+				FillUserData(driver, "", "Email@TestDomain.com", "12p@ssw3@rd", "12p@ssw3@rd", true);
+				
+				//Assertion of 'User Name is required'
+				Boolean Passed= AssertElement("user.name.error","Required");
+				if (Passed)
+				{
+					System.out.println("Assertion of 'User Name is required' is done successfully.");
+				}else
+				{
+					fail("Assertion of 'User Name is required' is failed.");
+				}
+	}
+
+	@Test 
+	public void UserNameIsUnique() throws Exception{
+		
+		// Open Browser
+		LoadBrowser();
+		
+		// Test Case #2: User Name Must be unique. 
+		// Fill in all data twice with same values of User Name
+		
+		// Check if we don't need data of all users, we will set UsingWebServiceDelete to true,
+		//else we will check if name is existed on users or need to generate same user twice.
+		
+		if (UsingWebServiceDelete == true){
+			// Delete All Users
+			sendDelete(baseURL+"/user/all");
+			System.out.print("All Users are deleted.");
+			FillUserData(driver, "UserUnique", "Email1@Domain.com", "12p@ssw3@rd", "12p@ssw3@rd", true);
+			
+			Thread.sleep(1000);
+			driver.navigate().to(baseURL);
+			FillUserData(driver, "UserUnique", "Email2@Domain.com", "12p@ssw3@rd", "12p@ssw3@rd", true);
+			
+			}
+		else {
+			
+			// We have two cases as we don't delete all users: If user exists and if not. 
+			// As Email should be unique too, we will create a random value twice and added them to email accounts
+			strValueOfEmailOne = RandomValue(10, 10000, "EmailOne", true);
+			strValueOfEmailTwo = RandomValue(10010, 20000, "EmailTwo", true);
+			
+			// Use Random Values
+			UserNameOne = RandomValue(100, 10000, "Name", false);
+			UserNameOneRepeated = UserNameOne;
+			
+			boolean NameExists = checkNameIsUnique(UserNameOneRepeated);
+			if (NameExists){  
+				// As the name is existing, so we will need to create a user with same data
+				System.out.print("User Name Found");
+				FillUserData(driver, UserNameOneRepeated, strValueOfEmailOne, "12p@ssw3@rd", "12p@ssw3@rd", true);
+			}
+			else
+			{
+				// User is not existing, we will create same name twice. 
+				// First Time	
+				FillUserData(driver, UserNameOne, strValueOfEmailOne, "12p@ssw3@rd", "12p@ssw3@rd", true);
+				Thread.sleep(2000);
+				driver.navigate().to(baseURL);
+				// Second Time
+				FillUserData(driver, UserNameOneRepeated, strValueOfEmailTwo, "12p@ssw3@rd", "12p@ssw3@rd", true);
+			}
+		
+		// Assertion of 'User Name Must be unique' 
+		Thread.sleep(2000);
+		Boolean Passed= AssertElement("user.name.error","Must be unique");
+		if (Passed)
+		{
+			System.out.println("Assertion of 'User Name is required' is done successfully.");
+		}else
+		{
+			fail("Assertion of 'User Name is required' is failed.");
+		}
+		}
+	}
+
+	@Test
+	public void UserEmailIsRequired(){
+				// Open Browser
+				LoadBrowser();
+				// Test Case #3: Email is required. 
+				// Fill in all data but set Email to be NULL
+				FillUserData(driver, "EmailIsRequired", "", "12p@ssw3@rd", "12p@ssw3@rd", true);
+				
+				//Assertion of 'Email is required'
+				Boolean Passed= AssertElement("user.email.error","Required");
+				if (Passed)
+				{
+					System.out.println("Assertion of 'Email is required' is done successfully.");
+				}else
+				{
+					fail("Assertion of 'Email is required' is failed.");
+				}
+	}
+	
+	@Test 
+	public void UserEmailIsUnique() throws Exception{
+		
+		// Open Browser
+		LoadBrowser();
+		
+		// Test Case #4: Email Must be unique. 
+		// Fill in all data twice with same values of Email
+		
+		// Check if we don't need data of all users, we will set UsingWebServiceDelete to true,
+		//else we will check if email is existed on users or need to generate same email twice.
+		
+		if (UsingWebServiceDelete == true){
+			// Delete All Users
+			sendDelete(baseURL+"/user/all");
+			System.out.print("All Users are deleted.");
+			FillUserData(driver, "EmailUnique1", "Email@Domain.com", "12p@ssw3@rd", "12p@ssw3@rd", true);
+			
+			Thread.sleep(1000);
+			driver.navigate().to(baseURL);
+			FillUserData(driver, "EmailUnique2", "Email@Domain.com", "12p@ssw3@rd", "12p@ssw3@rd", true);
+			
+			}
+		else {
+			
+			// We have two cases as we don't delete all users: If email exists and if not. 
+			// As Name should be unique too, we will create a random value twice and added them to user Name
+			strValueToAddToNameOne = RandomValue(5, 10000, "EmailUnique", false);
+			strValueToAddToNameTwo = RandomValue(10050, 50000, "EmailUnique", false);
+			
+			// Use Random Values
+			strEmail = RandomValue(10000, 999999, "EmailUnique", true);
+			strEmailRepeated = strEmail;
+			
+			boolean EmailExists = checkEmailIsUnique(strEmailRepeated);
+			if (EmailExists){  
+				// As the Email is existing, so we will need to create a user with same data
+				System.out.print("User Email Found");
+				FillUserData(driver, "EmailIsUnique"+strValueToAddToNameOne, strEmailRepeated, "12p@ssw3@rd", "12p@ssw3@rd", true);
+			}
+			else
+			{
+				// User is not existing, we will create same email twice. 
+				// First Time	
+				FillUserData(driver, "EmailIsUnique"+strValueToAddToNameOne, strEmail, "12p@ssw3@rd", "12p@ssw3@rd", true);
+				
+				Thread.sleep(2000);
+				driver.navigate().to(baseURL);
+				// Second Time
+				FillUserData(driver, "EmailIsUnique"+strValueToAddToNameTwo, strEmailRepeated, "12p@ssw3@rd", "12p@ssw3@rd", true);
+			}
+		
+		// Assertion of 'User Email Must be unique' 
+		Thread.sleep(2000);
+		//Assertion of 'Email is required'
+		Boolean Passed= AssertElement("user.email.error","Must be unique");
+		if (Passed)
+		{
+			System.out.println("Assertion of 'Email is required' is done successfully.");
+		}else
+		{
+			fail("Assertion of 'Email is required' is failed.");
+		}
+		}
+	}
+
+	@Test
+	public void UserPasswordIsRequired() throws InterruptedException{
+				// Open Browser
+				LoadBrowser();
+
+				strValueOfName = RandomValue(10, 10000, "Pass", false);
+				strValueOfEmail = RandomValue(10010, 20000, "Pass", true);
+				// Test Case #5: Password is required. 
+				// Fill in all data but set Password to be NULL
+				FillUserData(driver, strValueOfName, strValueOfEmail, "", "", true);
+				
+				//Assertion of 'Password is required'
+				Boolean Passed= AssertElement("user.password.error","Required");
+				if (Passed)
+				{
+					System.out.println("Assertion of 'Password is required' is done successfully.");
+				}else
+				{
+					fail("Assertion of 'Password is required' is failed.");
+				}
+				
+	}
+	
+	@Test
+	public void UserPasswordsNotSame() throws InterruptedException{
+				// Open Browser
+				LoadBrowser();
+
+				strValueOfName = RandomValue(10, 10000, "Pass", false);
+				strValueOfEmail = RandomValue(10010, 20000, "Pass", true);
+				// Test Case #6: Passwords are not the same. 
+				// Fill in all data but set Password a value which is different than the confirmation password.
+				FillUserData(driver, strValueOfName, strValueOfEmail, "12p@ssw3@rd", "nop@ssw3@rd", true);
+				
+				//Assertion of 'Password is required'
+				Boolean Passed= AssertElement("user.confirmationPassword.error","passwords are not the same");
+				if (Passed)
+				{
+					System.out.println("Assertion of 'passwords are not the same' is done successfully.");
+				}else
+				{
+					fail("Assertion of 'passwords are not the same' is failed.");
+				}
+				
+	}
+	
+}
